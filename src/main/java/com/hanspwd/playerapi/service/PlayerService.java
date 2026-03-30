06fd02c768622c64d49b1a7d8b21cbc9b37e027c
Hans <hwnss.w@gmail.com>
2026-03-30 00:46:07 -0300

    feat: PlayerService implemented

--- src/main/java/com/hanspwd/playerapi/service/PlayerService.java
+++ src/main/java/com/hanspwd/playerapi/service/PlayerService.java
@@ -1,5 +1,45 @@
 package com.hanspwd.playerapi.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hanspwd.playerapi.model.Player;
+import com.hanspwd.playerapi.repository.PlayerRepository;
+
+@Service
 public class PlayerService {
+
+    @Autowired
+    private PlayerRepository playerRepository;
     
+    public List<Player> getAllPlayers() {
+        return playerRepository.getAllPlayers();
+    } 
+
+    public List<Player> getAllBannedPlayers() {
+        return playerRepository.getAllBannedPlayers();
+    }
+
+    public Optional<Player> getPlayerById(int id) {
+        return playerRepository.getPlayerById(id);
+    }
+
+    public Player addPlayer(Player player) {
+        return playerRepository.addPlayer(player);
+    } 
+
+    public List<Player> addPlayers(List<Player> newPlayers) {
+        return playerRepository.addPlayers(newPlayers);
+    }
+
+    public void removePlayer(int id) {
+        playerRepository.removePlayer(id);
+    }
+
+    public Player updatePlayerById(int id, Player newPlayer) {
+        return playerRepository.updatePlayerById(id, newPlayer);
+    }
 }
