6285116419451bc50c4be28f169042c0f54982b9
Hans <hwnss.w@gmail.com>
2026-03-29 22:23:29 -0300

    feat: PlayerRepository implemented

--- src/main/java/com/hanspwd/playerapi/model/Player.java
+++ src/main/java/com/hanspwd/playerapi/model/Player.java
@@ -1,5 +1,21 @@
 package com.hanspwd.playerapi.model;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Player {
     
+    // id, username, level, isBanned, joinDate.
+    private int id;
+    private String username;
+    private long level;
+    private boolean isBanned;
+    private LocalDateTime joinDate;
+
 }
--- src/main/java/com/hanspwd/playerapi/repository/PlayerRepository.java
+++ src/main/java/com/hanspwd/playerapi/repository/PlayerRepository.java
@@ -1,5 +1,49 @@
 package com.hanspwd.playerapi.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.hanspwd.playerapi.model.Player;
+
+@Repository
 public class PlayerRepository {
     
+    private List<Player> players = new ArrayList<>();
+
+    public List<Player> getAllPlayers() {
+        return players;
+    }
+
+    public List<Player> getAllBannedPlayers() {
+        return players.stream().filter(p -> p.isBanned() == true).toList();
+    }
+
+    public Optional<Player> getPlayerById(int id ) {
+        return players.stream().filter(p -> p.getId() == id).findFirst();
+    }
+
+    public Player addPlayer(Player player) {
+        players.add(player);
+        return player;
+    }
+
+    public List<Player> addPlayers(List<Player> newPlayers) {
+        players.addAll(newPlayers);
+        return newPlayers;
+    }
+
+    public boolean removePlayer(int id) {
+        return players.removeIf(p -> p.getId() == id); 
+    }
+
+    public Player updatePlayerById(int id, Player newPlayer) {
+        Player oldPlayer = getPlayerById(id).get();
+        oldPlayer.setUsername(newPlayer.getUsername());
+        oldPlayer.setLevel(newPlayer.getLevel());
+        oldPlayer.setBanned(newPlayer.isBanned());
+        return oldPlayer;
+    }
 }
