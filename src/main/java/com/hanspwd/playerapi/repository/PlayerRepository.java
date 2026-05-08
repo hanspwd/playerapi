ed79b95afbb7aea81618c24d2bf502c34eb01f6a
Hans <hwnss.w@gmail.com>
2026-05-08 19:45:09 -0400

    @Repository anotation added

--- src/main/java/com/hanspwd/playerapi/repository/PlayerRepository.java
+++ src/main/java/com/hanspwd/playerapi/repository/PlayerRepository.java
@@ -1,10 +1,12 @@
 package com.hanspwd.playerapi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.hanspwd.playerapi.model.Player;
 
 // JpaRepository<Type, ID>)
+@Repository
 public interface PlayerRepository extends JpaRepository<Player, Long>{
     
 }
